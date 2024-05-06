20eab42ca6a5e8541ca1fd097ec6d4d37863612d
Ross <rossumj@hotmail.com>
2024-05-06 11:17:26 -0400

    Begin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './structures/header/header.component';
+import { FooterComponent } from './structures/footer/footer.component';
+import { CardComponent } from './structures/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
